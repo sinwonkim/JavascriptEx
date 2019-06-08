855593c1eda881d20b9f2954c83a3e2e93efd57a
sinwonkim <zhfps114@gmail.com>
2019-06-08 23:32:52 +0900

    # jacascirpt1

--- operator.js
+++ operator.js
@@ -1,14 +1,20 @@
-let getScore = 80;  // 획득 점수 
-let msg = ''; // 메시지
+let scores = [24,32,17]; // 점수의 배열 
+let arrayLength = scores.length; // 배열 내의 아이템의 개수 
+let roundNumber = 0; // 현재 응시 회차 
+var msg = ""; // 메시지
+var i; //카운터 
 
-let  addMsg = function() {
- msg += '합격을 축하 합니다.';
-};
+// 배열의 아이템 개수만큼 루프를 실행한다.
+for(var i = 0; i < arrayLength; i++){
+    // 배열의 인덱스는 0 부터 시작한다. 
+    roundNumber = (i + 1);
 
-if( getScore >= 70){ // 내가 획득한 점수가 높은게 맞으면 조건식이 true 면 addMsg() 함수를 실행해서 합격 축하 메시지를 msg에 더하기 연산을 해줘라
-    addMsg();
+    // 현재 회차를 메시지에 추가한다. 
+
+    msg += "응시회차" + roundNumber + " : ";
+
+    // 점수 배열에서 점수를 얻어온다.
+    msg += scores[i] + "<br/>";
 }
 
-let el = document.getElementById("checkPassContenteck");
-el.innerHTML = msg;
-
+document.getElementById("checkPassContenteck").innerHTML = msg ;
